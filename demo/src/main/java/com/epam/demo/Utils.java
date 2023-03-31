911a032d79be1f9464c1745cd39b7b0809f1ba3c
AliNajimov <najimovm11@gmail.com>
2023-03-31 10:06:28 +0500

    null

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
+        if (args == null) {
+            return false;
+        }
         for (String s : args) {
             if (!StringUtils.isPositiveNumber(s)) {
                 return false;
