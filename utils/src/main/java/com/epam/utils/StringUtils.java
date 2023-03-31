94a754f6f40663c85f1f2214f03316850b8bd3e0
AliNajimov <najimovm11@gmail.com>
2023-03-31 09:20:26 +0500

    fixed

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,5 +1,7 @@
 package com.epam.utils;
 
+import static org.apache.commons.lang3.time.DurationUtils.isPositive;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         //code!
