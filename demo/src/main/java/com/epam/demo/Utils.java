7b8249b47221dd3c8bfdb250df4c557e55ee987e
AliNajimov <najimovm11@gmail.com>
2023-03-31 10:21:04 +0500

    done

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        if (args == null){
+        if (args.isEmpty()){
             return false;
         }
         return args.stream().allMatch(a ->a.matches("\\d+") && Double.parseDouble(a)>0);
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,7 +2,7 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        if(str == null){
+        if(str.isEmpty()){
             return false;
         }
         double v = Double.parseDouble(str);
